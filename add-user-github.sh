596152062ce3e73eefdd5b1d3e85dd47e4331853
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-09-10 01:08:32 +0200

    update add-user-github.sh

--- add-user-github.sh
+++ add-user-github.sh
@@ -14,11 +14,11 @@ else
     adduser -m "$USERNAME"
     echo "$USERNAME:$PASSWORD" | chpasswd
     usermod -aG sudo "$USERNAME"
-
     echo "User '$USERNAME' created and added to the sudoers group."
 fi
 
 SSH_DIR="/home/$USERNAME/.ssh"
+
 if [ ! -d "$SSH_DIR" ]; then
     sudo -u "$USERNAME" mkdir "$SSH_DIR"
     sudo -u "$USERNAME" chmod 700 "$SSH_DIR"
