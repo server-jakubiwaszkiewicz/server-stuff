f096e9b3b58ffd0a49d8cf47cf39d63934ea67a0
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-09-10 00:10:52 +0200

    rename 1 file and update 1 file

--- add-user-github.sh
+++ add-user-github.sh
@@ -2,9 +2,9 @@ echo "Do you want to add user called github? (y/n)"
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    username = "github"
-    password = "RyOhdhKA1"
-	useradd -m -p "$pass" "$username"
+    username = github
+    password = RyOhdhKA1
+	useradd -m -p $pass $username
     usermod -aG sudo github
     echo "Adding SSH key..."
     mkdir /home/github/.ssh && touch /home/github/.ssh/authorized_keys && chmod 700 /home/github/.ssh && chmod 600 /home/github/.ssh/authorized_keys
--- install.sh
+++ install.sh
@@ -50,17 +50,7 @@ echo "Do you want to add user called github? (y/n)"
 read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    username = "github"
-    password = "RyOhdhKA1"
-	useradd -m -p "$pass" "$username"
-    usermod -aG sudo github
-    echo "Adding SSH key..."
-    mkdir /home/github/.ssh && touch /home/github/.ssh/authorized_keys && chmod 700 /home/github/.ssh && chmod 600 /home/github/.ssh/authorized_keys
-    ssh-keygenm -t ed25519 -C "kkubaiwaszkiewicz@gmail.com"
-    cat /home/github/.ssh/id_ed25519.pub >> /home/github/.ssh/authorized_keys
-    cat /home/github/.ssh/id_ed25519
+    ./add-user-github.sh
 else
     echo "Exiting..."
 fi
-
-echo "Download "
