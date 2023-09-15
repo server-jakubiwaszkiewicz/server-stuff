87a28a8326806e7a9a1f5bcb1aa373b5a40077c9
jakubiwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-09-16 01:35:55 +0200

    update caddy.json and clear-docker.sh

--- scripts_for_project_directiories/clear-docker.sh
+++ scripts_for_project_directiories/clear-docker.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Get the parent folder name
-parent_folder_name="$(basename "$(pwd)")"
+# Define the image name you want to search for
+IMAGE_NAME="your_image_name_here"
 
-# Specify the image name based on the parent folder name
-image_name="image-$parent_folder_name"
+# Get the container ID(s) based on the image name
+CONTAINER_IDS=$(docker ps -q --filter "ancestor=${IMAGE_NAME}")
 
-# Specify the container name based on the parent folder name
-container_name="$parent_folder_name"
-
-# Stop and remove the Docker container
-docker stop "$container_name" && docker rm "$container_name"
-
-# Remove the Docker image
-docker rmi "$image_name"
+# Check if any containers were found
+if [ -z "$CONTAINER_IDS" ]; then
+  echo "No containers found with the image name: ${IMAGE_NAME}"
+else
+  # Loop through each container and stop/remove it
+  for CONTAINER_ID in $CONTAINER_IDS; do
+    echo "Stopping and removing container ID: ${CONTAINER_ID}"
+    docker stop "$CONTAINER_ID"  # Stop the container
+    docker rm "$CONTAINER_ID"    # Remove the container
+  done
+fi
